9b9f3ae8e251f32e7f66b0527f9b12993de28131
luckyardiantom <47424591+luckyardiantom@users.noreply.github.com>
2019-04-25 16:41:39 +0700

    Add files via upload

--- Lapor.java
+++ Lapor.java
@@ -1,16 +1,10 @@
 package com.aspirasibandung.aspirasibandung;
 
 import android.annotation.SuppressLint;
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -21,15 +15,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -44,24 +33,10 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-
-import java.io.IOException;
-import java.util.UUID;
 
 public class Lapor extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private Button PilihGambar, Upload;
-    private ImageView Img;
-    private EditText edtKategori, edtTanggal, edtDesk;
-    private Uri filePath;
-    FirebaseStorage storage;
-    StorageReference storageReference;
-    private final int PICK_IMAGE_REQUEST = 71;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,84 +54,10 @@ public class Lapor extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        storage = FirebaseStorage.getInstance();
-        storageReference = storage.getReference();
-        PilihGambar = (Button) findViewById(R.id.PilihGambar);
-        Upload = (Button) findViewById(R.id.Upload);
-        Img = (ImageView) findViewById(R.id.Img);
-        edtDesk = (EditText) findViewById(R.id.edtDesk);
-        edtTanggal = (EditText) findViewById(R.id.edtTanggal);
-        edtKategori = (EditText) findViewById(R.id.edtKategori);
 
-        PilihGambar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                chooseImage();
-            }
-        });
 
-        Upload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                uploadImage();
-            }
-        });
     }
 
-    private void chooseImage() {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Pilih Gambar"), PICK_IMAGE_REQUEST);
-    }
-
-    private void uploadImage() {
-
-        if (filePath != null) {
-            final ProgressDialog progressDialog = new ProgressDialog(this);
-            progressDialog.setTitle("Uploading...");
-            progressDialog.show();
-
-            StorageReference ref = storageReference.child("images/" + UUID.randomUUID().toString());
-            ref.putFile(filePath)
-                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            progressDialog.dismiss();
-                            Toast.makeText(Lapor.this, "Uploaded", Toast.LENGTH_SHORT).show();
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            progressDialog.dismiss();
-                            Toast.makeText(Lapor.this, "Failed " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    })
-                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot
-                                    .getTotalByteCount());
-                            progressDialog.setMessage("Uploaded " + (int) progress + "%");
-                        }
-                    });
-        }
-
-        if (TextUtils.isEmpty(edtKategori.getText()) || TextUtils.isEmpty(edtTanggal.getText()) || TextUtils.isEmpty(edtDesk.getText())) {
-            Toast.makeText(Lapor.this, "Data yang diminta tidak boleh kosong", Toast.LENGTH_SHORT).show();
-        } else {
-            //instansiasi database firebase
-            FirebaseDatabase database = FirebaseDatabase.getInstance();
-
-            //Referensi database yang dituju
-            DatabaseReference referensi = database.getReference("Lapor").child(edtTanggal.getText().toString());
-
-            //memberi nilai pada referensi yang dituju
-            referensi.child("Deskripsi").setValue(edtDesk.getText().toString());
-            referensi.child("Kategori").setValue(edtKategori.getText().toString());
-        }
-    }
 
     @Override
     public void onBackPressed() {
@@ -165,24 +66,6 @@ public class Lapor extends AppCompatActivity
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
-                && data != null && data.getData() != null )
-        {
-            filePath = data.getData();
-            try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
-                Img.setImageBitmap(bitmap);
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
         }
     }
 
