db6c104cd3ebb7cd3c2a56353ddbd7c793ced497
ilhamfannani <47444329+ilhamfannani@users.noreply.github.com>
2019-04-26 14:47:11 +0700

    #11

--- SplashScreen.java
+++ SplashScreen.java
@@ -1,9 +1,10 @@
-package com.example.aspirasilapor;
+package com.aspirasibandung.aspirasibandung;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
