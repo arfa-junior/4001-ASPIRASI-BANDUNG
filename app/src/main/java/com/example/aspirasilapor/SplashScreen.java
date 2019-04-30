26edd96f6ad7caab42750903af884e65e1508db6
Muhammad Iqbal Muttaqin <iqbalmuttaqin76@gmail.com>
2019-04-30 11:52:27 +0700

    ini commit fix pertama home #3

--- app/src/main/java/com/example/aspirasilapor/SplashScreen.java
+++ app/src/main/java/com/example/aspirasilapor/SplashScreen.java
@@ -1,48 +1,43 @@
 package com.example.aspirasilapor;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.Window;
+import android.support.v7.app.ActionBar;
 import android.view.WindowManager;
 
-public class SplashScreen extends Activity {
+import com.daimajia.androidanimations.library.Techniques;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
 
-    //Set waktu lama splashscreen
-    private static int splashInterval = 3000;
 
+public class SplashScreen extends AwesomeSplash {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    public void initSplash(ConfigSplash configSplash){
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//
+        //menambahkan background
+        configSplash.setBackgroundColor(R.color.bg_splash);
+        configSplash.setAnimCircularRevealDuration(3000);
+        configSplash.setRevealFlagX(Flags.REVEAL_LEFT);
+        configSplash.setRevealFlagX(Flags.REVEAL_BOTTOM);
 
-        setContentView(R.layout.activity_splash_screen);
+        //menambahkan logo
+        configSplash.setLogoSplash(R.drawable.logo);
+        configSplash.setAnimLogoSplashDuration(2000);
+        configSplash.setAnimTitleTechnique(Techniques.FadeInUp);
 
-        new Handler().postDelayed(new Runnable() {
-
-
-            @Override
-            public void run() {
-                // TODO Auto-generated method stub
-                Intent i = new Intent(SplashScreen.this, TampilanAwal.class);
-                startActivity(i);
-
-
-                //jeda selesai Splashscreen
-                this.finish();
-            }
-
-            private void finish() {
-                // TODO Auto-generated method stub
-
-            }
-        }, splashInterval);
-
+        //menambahkan title
+        configSplash.setTitleSplash("Bandung Juara");
+        configSplash.setTitleTextColor(R.color.splash_title);
+        configSplash.setAnimTitleDuration(1000);
+        configSplash.setTitleTextSize(20f);
+        configSplash.setAnimTitleDuration(1000);
+        configSplash.setAnimTitleTechnique(Techniques.FadeIn);
     }
-
-
-
+    @Override
+    public void animationsFinished(){
+        startActivity(new Intent(SplashScreen.this, Login.class));
+    }
 }
