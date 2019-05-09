82635360efcd7f1b25083194d1d738479a423292
ilhamfannani <47444329+ilhamfannani@users.noreply.github.com>
2019-05-09 08:52:06 +0700

    Add files via upload

--- CardLapor.java
+++ CardLapor.java
@@ -34,4 +34,226 @@ public class CardLapor {
     public void setdeskripsi(String Deskripsi) {
         this.deskripsi = Deskripsi;
     }
+    ///
+    //
+    //
+    //
+    //
+    ///
+    ////
+    ///
+    ///
+    //
+    ///
+    //
+    //
+    ///
+    ///
+    ///
+    ///
+    package com.example.aspirasilapor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+    public class Card {
+        String imagePath,kategori,tanggal,deskripsi;
+
+        public Card(String imagePath, String nama, String harga, String deskripsi) {
+            this.imagePath = imagePath;
+            this.kategori = nama;
+            this.tanggal = harga;
+            this.deskripsi = deskripsi;
+        }
+
+        public String getImagePath() {
+            return imagePath;
+        }
+
+        public void setImagePath(String imagePath) {
+            this.imagePath = imagePath;
+        }
+
+        public String getKategori() {
+            return kategori;
+        }
+
+        public void setKategoria(String kategori) {
+            this.kategori = kategori;
+        }
+
+        public String getTanggal() {
+            return tanggal;
+        }
+
+        public void setTanggal(String tanggal) {
+            this.tanggal = tanggal;
+        }
+
+        public String getDeskripsi() {
+            return deskripsi;
+        }
+
+        public void setDeskripsi(String deskripsi) {
+            this.deskripsi = deskripsi;
+        }
+    }
+
+    //
+    //
+    ///
+    ///
+    ///
+    ////
+
+
+    ///////////////
+    //
+    package com.example.aspirasilapor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+    public class Card {
+        String imagePath,kategori,tanggal,deskripsi;
+
+        public Card(String imagePath, String nama, String harga, String deskripsi) {
+            this.imagePath = imagePath;
+            this.kategori = nama;
+            this.tanggal = harga;
+            this.deskripsi = deskripsi;
+        }
+
+        public String getImagePath() {
+            return imagePath;
+        }
+
+        public void setImagePath(String imagePath) {
+            this.imagePath = imagePath;
+        }
+
+        public String getKategori() {
+            return kategori;
+        }
+
+        public void setKategoria(String kategori) {
+            this.kategori = kategori;
+        }
+
+        public String getTanggal() {
+            return tanggal;
+        }
+
+        public void setTanggal(String tanggal) {
+            this.tanggal = tanggal;
+        }
+
+        public String getDeskripsi() {
+            return deskripsi;
+        }
+
+        public void setDeskripsi(String deskripsi) {
+            this.deskripsi = deskripsi;
+        }
+    }
+
+    package com.example.aspirasilapor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+    public class Card {
+        String imagePath,kategori,tanggal,deskripsi;
+
+        public Card(String imagePath, String nama, String harga, String deskripsi) {
+            this.imagePath = imagePath;
+            this.kategori = nama;
+            this.tanggal = harga;
+            this.deskripsi = deskripsi;
+        }
+
+        public String getImagePath() {
+            return imagePath;
+        }
+
+        public void setImagePath(String imagePath) {
+            this.imagePath = imagePath;
+        }
+
+        public String getKategori() {
+            return kategori;
+        }
+
+        public void setKategoria(String kategori) {
+            this.kategori = kategori;
+        }
+
+        public String getTanggal() {
+            return tanggal;
+        }
+
+        public void setTanggal(String tanggal) {
+            this.tanggal = tanggal;
+        }
+
+        public String getDeskripsi() {
+            return deskripsi;
+        }
+
+        public void setDeskripsi(String deskripsi) {
+            this.deskripsi = deskripsi;
+        }
+    }
+
+
+    package com.example.aspirasilapor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+    public class Card {
+        String imagePath,kategori,tanggal,deskripsi;
+
+        public Card(String imagePath, String nama, String harga, String deskripsi) {
+            this.imagePath = imagePath;
+            this.kategori = nama;
+            this.tanggal = harga;
+            this.deskripsi = deskripsi;
+        }
+
+        public String getImagePath() {
+            return imagePath;
+        }
+
+        public void setImagePath(String imagePath) {
+            this.imagePath = imagePath;
+        }
+
+        public String getKategori() {
+            return kategori;
+        }
+
+        public void setKategoria(String kategori) {
+            this.kategori = kategori;
+        }
+
+        public String getTanggal() {
+            return tanggal;
+        }
+
+        public void setTanggal(String tanggal) {
+            this.tanggal = tanggal;
+        }
+
+        public String getDeskripsi() {
+            return deskripsi;
+        }
+
+        public void setDeskripsi(String deskripsi) {
+            this.deskripsi = deskripsi;
+        }
+    }
+
+
+
 }
--- SplashScreen.java
+++ SplashScreen.java
@@ -1,5 +1,52 @@
-package com.aspirasibandung.aspirasibandung;
+package com.example.aspirasilapor;
 
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends Activity {
+
+    //Set waktu lama splashscreen
+    private static int splashInterval = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(SplashScreen.this, TampilanAwal.class);
+                startActivity(i);
+
+
+                //jeda selesai Splashscreen
+                this.finish();
+            }
+
+            private void finish() {
+                // TODO Auto-generated method stub
+
+            }
+        }, splashInterval);
+
+    }
+
+
+
+=======
 import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.view.WindowManager;
@@ -40,4 +87,4 @@ public class SplashScreen extends AwesomeSplash {
     public void animationsFinished(){
         startActivity(new Intent(SplashScreen.this, Login.class));
     }
-}
+
