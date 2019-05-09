8d4cba124581ceb8e4d1d00c1505761956a5675c
ilhamfannani <47444329+ilhamfannani@users.noreply.github.com>
2019-05-09 09:36:52 +0700

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,18 +1,7 @@
-package com.aspirasibandung.aspirasibandung;
+package com.example.aspirasilapor;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.view.menu.MenuAdapter;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -21,43 +10,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.firestore.DocumentChange;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.squareup.picasso.Picasso;
-
-import org.w3c.dom.Comment;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    Firebase fb;
+    ArrayList<String> tanggal = new ArrayList<>();
+    ListView lv;
 
-    private DatabaseReference mDatabase;
-    Button load;
-    RecyclerView rvMain;
-    List<CardLapor> List;
-    AdapterLapor myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,45 +46,42 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        rvMain = findViewById(R.id.rvMain);
-        rvMain.setLayoutManager(new LinearLayoutManager(this));
-        List = new ArrayList<>();
-        myAdapter = new AdapterLapor(List, MainActivity.this);
-        rvMain.setAdapter(myAdapter);
+        Firebase.setAndroidContext(this);
 
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        fb = new Firebase("https://aspirasistore.firebaseio.com/Lapor/Tanggal");
+        lv = (ListView) findViewById(R.id.Listview);
 
-        load = findViewById(R.id.load);
-        load.setOnClickListener(new View.OnClickListener() {
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,tanggal);
+
+        lv.setAdapter(arrayAdapter);
+        fb.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onClick(View v) {
-                loadData();
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String value = dataSnapshot.getValue(String.class);
+                tanggal.add(value);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
             }
         });
-    }
-
-    private void loadData() {
-        List.clear();
-        mDatabase.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for (DataSnapshot ds : dataSnapshot.child("Lapor").getChildren()){
-                    CardLapor lapor = new CardLapor();
-                    lapor.setdeskripsi(String.valueOf(ds.child("Deskripsi").getValue()));
-                    lapor.setkategori(String.valueOf(ds.child("Kategori").getValue()));
-                    lapor.setimg(String.valueOf(ds.child("Image").getValue()));
-                    List.add(lapor);
-                }
-                myAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-
-
     }
 
 
@@ -150,10 +118,9 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-
     private void logout(){
         FirebaseAuth.getInstance().signOut();
-        Intent intent = new Intent(getApplicationContext(), Login.class);
+        Intent intent = new Intent(getApplicationContext(), TampilanAwal.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         Toast.makeText(MainActivity.this, "Thanks for visited", Toast.LENGTH_SHORT).show();
         startActivity(intent);
@@ -223,3 +190,198 @@ public class MainActivity extends AppCompatActivity
     }
 
 }
+//package com.example.aspirasilapor;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.support.design.widget.NavigationView;
+//import android.support.v4.view.GravityCompat;
+//import android.support.v4.widget.DrawerLayout;
+//import android.support.v7.app.ActionBarDrawerToggle;
+//import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.Toolbar;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//import android.widget.ArrayAdapter;
+//import android.widget.ListView;
+//import android.widget.Toast;
+//
+//import com.firebase.client.ChildEventListener;
+//import com.firebase.client.DataSnapshot;
+//import com.firebase.client.Firebase;
+//import com.firebase.client.FirebaseError;
+//import com.google.firebase.auth.FirebaseAuth;
+//
+//import java.util.ArrayList;
+//
+//public class MainActivity extends AppCompatActivity
+//        implements NavigationView.OnNavigationItemSelectedListener {
+//
+//    Firebase fb;
+//    ArrayList<String> tanggal = new ArrayList<>();
+//    ListView lv;
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//        drawer.addDrawerListener(toggle);
+//        toggle.syncState();
+//
+//        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+//        navigationView.setNavigationItemSelectedListener(this);
+//
+//        Firebase.setAndroidContext(this);
+//
+//        fb = new Firebase("https://aspirasistore.firebaseio.com/Lapor/Tanggal");
+//        lv = (ListView) findViewById(R.id.Listview);
+//
+//        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,tanggal);
+//
+//        lv.setAdapter(arrayAdapter);
+//        fb.addChildEventListener(new ChildEventListener() {
+//            @Override
+//            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+//                String value = dataSnapshot.getValue(String.class);
+//                tanggal.add(value);
+//                arrayAdapter.notifyDataSetChanged();
+//            }
+//
+//            @Override
+//            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+//
+//            }
+//
+//            @Override
+//            public void onChildRemoved(DataSnapshot dataSnapshot) {
+//
+//            }
+//
+//            @Override
+//            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(FirebaseError firebaseError) {
+//
+//            }
+//        });
+//    }
+//
+//
+//
+//    @Override
+//    public void onBackPressed() {
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        if (drawer.isDrawerOpen(GravityCompat.START)) {
+//            drawer.closeDrawer(GravityCompat.START);
+//        } else {
+//            super.onBackPressed();
+//        }
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+//
+//    private void logout(){
+//        FirebaseAuth.getInstance().signOut();
+//        Intent intent = new Intent(getApplicationContext(), TampilanAwal.class);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        Toast.makeText(MainActivity.this, "Thanks for visited", Toast.LENGTH_SHORT).show();
+//        startActivity(intent);
+//
+//    }
+//    private void aboutus(){
+//        Intent intent = new Intent(getApplicationContext(), AboutUs.class);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        startActivity(intent);
+//    }
+//    private void feedback(){
+//        Intent intent = new Intent(getApplicationContext(), Feedback.class);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        startActivity(intent);
+//    }
+//    private void petunjukpenggunaan(){
+//        Intent intent = new Intent(getApplicationContext(), PetunjukPenggunaan.class);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        startActivity(intent);
+//    }
+//    private void notlpdarurat(){
+//        Intent intent = new Intent(getApplicationContext(), Notlpdarurat.class);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        startActivity(intent);
+//    }
+//    private void lapor(){
+//        Intent intent = new Intent(getApplicationContext(), Lapor.class);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        startActivity(intent);
+//    }
+//    private void statuslaporan(){
+//        Intent intent = new Intent(getApplicationContext(), StatusLaporan.class);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        startActivity(intent);
+//    }
+//
+//    @SuppressWarnings("StatementWithEmptyBody")
+//    @Override
+//    public boolean onNavigationItemSelected(MenuItem item) {
+//        // Handle navigation view item clicks here.
+//        int id = item.getItemId();
+//
+//        if (id == R.id.nav_lapor) {
+//            lapor();
+//
+//        } else if (id == R.id.nav_nomortelepondarurat) {
+//            notlpdarurat();
+//
+//        } else if (id == R.id.nav_petunjukpenggunaan) {
+//            petunjukpenggunaan();
+//
+//        } else if (id == R.id.nav_feedback) {
+//            feedback();
+//        } else if (id == R.id.nav_aboutus) {
+//            aboutus();
+//
+//        } else if (id == R.id.keluar) {
+//            logout();
+//        }
+//        else if (id == R.id.nav_statuslaporan) {
+//            statuslaporan();
+//        }
+//
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        drawer.closeDrawer(GravityCompat.START);
+//        return true;
+//    }
+//
+//}
+
+
+
--- StatusLaporan.java
+++ StatusLaporan.java
@@ -1,10 +1,7 @@
-package com.aspirasibandung.aspirasibandung;
+package com.example.aspirasilapor;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -26,6 +23,8 @@ public class StatusLaporan extends AppCompatActivity
         setContentView(R.layout.activity_status_laporan);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -70,7 +69,7 @@ public class StatusLaporan extends AppCompatActivity
     }
     private void logout(){
         FirebaseAuth.getInstance().signOut();
-        Intent intent = new Intent(getApplicationContext(), Login.class);
+        Intent intent = new Intent(getApplicationContext(), TampilanAwal.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         Toast.makeText(StatusLaporan.this, "Thanks for visited", Toast.LENGTH_SHORT).show();
         startActivity(intent);
@@ -133,7 +132,6 @@ public class StatusLaporan extends AppCompatActivity
         } else if (id == R.id.keluar) {
             logout();
         }
-
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
